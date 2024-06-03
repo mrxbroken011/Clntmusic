d7c2e6b99d0c1cc3f96670eb2cbfaf5fd942b7bb
Mr Broken <74880754+asad0111@users.noreply.github.com>
2024-06-03 14:51:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Asad0111/AnonXMusic",
+    "https://github.com/Asad0111/Brx1nMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
