c7f0b648da5bc95d5b01262f27ea285a54fcb919
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-15 03:59:26 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -11,7 +11,7 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="AnonXAss1",
+            name="Riya_cpl",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
