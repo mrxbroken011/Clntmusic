c1e56d68fcbdc7b7b2253e0ef6d0806a350da222
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-17 21:32:45 +0530

    Update welcome.py

--- AnonXMusic/plugins/tools/welcome.py
+++ AnonXMusic/plugins/tools/welcome.py
@@ -16,7 +16,7 @@ from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from logging import getLogger
-from AnonXMusic.utils.daxx_ban import admin_filter
+from AnonXMusic.utils.broken_ban import admin_filter
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
